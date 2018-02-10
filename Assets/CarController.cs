45369ac92b062f1a3a891485e422ed45437a87d1
robidev <robin.dev@gmail.com>
2018-02-11 00:07:21 +0100

    initial commit

---
+++ Assets/CarController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarController : MonoBehaviour {
+	public List<AxleInfo> axleInfos; // the information about each individual axle
+	public float maxMotorTorque; // maximum torque the motor can apply to wheel
+	public float maxSteeringAngle; // maximum steer angle the wheel can have
+	public float myMotor = 0;
+
+	public void FixedUpdate()
+	{
+	    float motor = (maxMotorTorque * Input.GetAxis("Vertical")) + myMotor;
+	    float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+	    //Debug.Log("steeringval:" + steering.ToString());
+
+	    foreach (AxleInfo axleInfo in axleInfos) {
+		/*if (axleInfo.steering) {
+		    axleInfo.leftWheel.steerAngle = steering;
+		    axleInfo.rightWheel.steerAngle = steering;
+		}*/
+		if (axleInfo.motor) {
+		    axleInfo.leftWheel.motorTorque = motor + steering;
+		    axleInfo.rightWheel.motorTorque = motor - steering;
+		}
+	    }
+	}
+}
+
+[System.Serializable]
+public class AxleInfo {
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool motor; // is this wheel attached to motor?
+	public bool steering; // does this wheel apply steer angle?
+}
---
+++ Assets/NetworkListner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkListner : MonoBehaviour {
+	public GameObject player;
+	public UDPRT myUDPRT;
+	// Use this for initialization
+	void Start () {
+		myUDPRT = UDPRT.CreateInstance(5000);
+		Debug.Log("listening on UDP 5000");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(UDPRT.ReceivedMsg != null){
+			Debug.Log("Received udp:" + UDPRT.ReceivedMsg);
+			if(UDPRT.ReceivedMsg == "forward"){
+				Debug.Log("forward");
+				var car = player.GetComponent<CarController>();
+				if (car != null)
+					car.myMotor = 4;
+				else
+					Debug.Log("cannot find car");
+			}
+			if(UDPRT.ReceivedMsg == "back"){
+				Debug.Log("forward");
+				var car = player.GetComponent<CarController>();
+				if (car != null)
+					car.myMotor = -4;
+				else
+					Debug.Log("cannot find car");
+			}
+			if(UDPRT.ReceivedMsg == "stop"){
+				Debug.Log("forward");
+				var car = player.GetComponent<CarController>();
+				if (car != null)
+					car.myMotor = 0;
+				else
+					Debug.Log("cannot find car");
+			}
+			UDPRT.ReceivedMsg = null;
+		}
+	}
+	void OnDestroy() {
+		Destroy(myUDPRT);
+		Debug.Log("NetworkListner was destroyed");
+	}
+}
